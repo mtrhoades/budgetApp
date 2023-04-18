e64c9529e26bb33d35fde109469927b744a9560d
root <mtrhoades0727@gmail.com>
2023-04-17 17:00:12 -1000

    starting setup of App

---
+++ frontend/budget/src/App.js
@@ -0,0 +1,16 @@
+// imports
+import './App.css';
+import Expenses from "./components/Expenses";
+import Income from "./components/Income";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Income />
+      <Expenses />
+    </div>
+  );
+}
+
+export default App;
