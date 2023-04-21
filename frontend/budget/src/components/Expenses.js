ecbcd08f51092ffe8c211c5a6b54e2328a584fb1
root <mtrhoades0727@gmail.com>
2023-04-20 16:38:50 -1000

    added expense component with add expense modal

--- frontend/budget/src/components/Expenses.js
+++ frontend/budget/src/components/Expenses.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import AddExpenseModal from './AddExpenseModal';
 
 
 const Expenses = () => {
   return (
-    <div>Expenses</div>
+    <div>Expenses
+
+    <AddExpenseModal />
+
+    <table class="w-50 table table-danger .table-hover .table-bordered border-danger text-center">
+        <thead>
+            <tr style={{}}>
+                <th scope="col">Expense</th>
+                <th scope="col">Amount</th>
+                <th scope="col">Date Due</th>
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
+
+
+
+
+    </div>
   )
 }
 
